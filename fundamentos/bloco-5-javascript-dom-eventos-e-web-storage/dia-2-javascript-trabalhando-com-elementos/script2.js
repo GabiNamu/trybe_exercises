c1c2915c66e16c700b740c57534882a6d51d991b
Gabriela Muniz <gabrielanamu@live.com>
2022-08-27 07:15:57 -0300

    execicios dia 5.2 não finalizados

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script2.js
@@ -0,0 +1,20 @@
+document.getElementById("elementoOndeVoceEsta").innerHTML;
+ document.getElementById("elementoOndeVoceEsta").parentElement.style.color = "purple";
+document.getElementById("primeiroFilhoDoFilho").innerHTML = "kbdkjbfksjndjfkjbkjubfjdkbuskudjbkjfbsdkbkjdfsbfkjsdbfjdbfjdksbffdku"
+
+console.log(document.getElementById("pai").firstElementChild);
+
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
+
+console.log(document.getElementById("pai").children[2]);
+
+let pai = document.getElementById("pai");
+
+let newsibling = document.createElement('section');
+
+
+
