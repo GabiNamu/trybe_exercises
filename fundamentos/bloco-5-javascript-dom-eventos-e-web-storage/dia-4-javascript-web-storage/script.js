99be8f848b7c1368976e14b337e682222b48d77d
Gabriela Muniz <gabrielanamu@live.com>
2022-09-06 18:14:51 -0300

    exercicios do dia 5.4

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,9 @@
+let la = document.getElementsByTagName('body');
+console.log("let rosa = document.getElementById(rosa)");
+rosa.addEventListener("click", mudaCor);
+
+function mudaCor() {
+    console.log(document.getElementsByTagName('body').style.backgroundColor = "pink"); 
+    
+}
+
